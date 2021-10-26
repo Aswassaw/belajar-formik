5a8c254e11801941f05f86003728d8a6b0ae4477
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-27 01:55:54 +0700

    13. Formik component

--- src/components/3.reducing-boilerplate/YoutubeForm.jsx
+++ src/components/3.reducing-boilerplate/YoutubeForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
@@ -10,11 +10,6 @@ const initialValues = {
 };
 
 // Validate with yup package
-const onSubmit = (values) => {
-  // e.preventDefault sudah otomatis diatasi oleh formik.
-  console.log(values);
-};
-
 const validationSchema = Yup.object({
   name: Yup.string().required("Name is required!"),
   email: Yup.string()
@@ -23,53 +18,58 @@ const validationSchema = Yup.object({
   channel: Yup.string().required("Channel is required!"),
 });
 
+const onSubmit = (values) => {
+  // e.preventDefault sudah otomatis diatasi oleh formik
+  console.log(values);
+};
+
 const YoutubeForm = () => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-  });
-
   return (
-    <form className='form' onSubmit={formik.handleSubmit}>
-      <label htmlFor='name'>Name</label>
-      <input
-        type='text'
-        name='name'
-        id='name'
-        {...formik.getFieldProps("name")}
-      />
-      {formik.touched.name && formik.errors.name && (
-        <div className='error-msg'>{formik.errors.name}</div>
-      )}
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <form className='form' onSubmit={formik.handleSubmit}>
+        <label htmlFor='name'>Name</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          {...formik.getFieldProps("name")}
+        />
+        {formik.touched.name && formik.errors.name && (
+          <div className='error-msg'>{formik.errors.name}</div>
+        )}
 
-      <label htmlFor='email'>Email</label>
-      <input
-        type='email'
-        email='email'
-        id='email'
-        {...formik.getFieldProps("email")}
-      />
-      {formik.touched.email && formik.errors.email && (
-        <div className='error-msg'>{formik.errors.email}</div>
-      )}
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          email='email'
+          id='email'
+          {...formik.getFieldProps("email")}
+        />
+        {formik.touched.email && formik.errors.email && (
+          <div className='error-msg'>{formik.errors.email}</div>
+        )}
 
-      <label htmlFor='channel'>Channel</label>
-      <input
-        type='text'
-        channel='channel'
-        id='channel'
-        {...formik.getFieldProps("channel")}
-      />
-      {formik.touched.channel && formik.errors.channel && (
-        <div className='error-msg'>{formik.errors.channel}</div>
-      )}
+        <label htmlFor='channel'>Channel</label>
+        <input
+          type='text'
+          channel='channel'
+          id='channel'
+          {...formik.getFieldProps("channel")}
+        />
+        {formik.touched.channel && formik.errors.channel && (
+          <div className='error-msg'>{formik.errors.channel}</div>
+        )}
 
-      <button type='submit'>Submit</button>
-      <button type='reset' onClick={formik.handleReset}>
-        Reset
-      </button>
-    </form>
+        <button type='submit'>Submit</button>
+        <button type='reset' onClick={formik.handleReset}>
+          Reset
+        </button>
+      </form>
+    </Formik>
   );
 };
 
