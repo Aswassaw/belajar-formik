b8982ee41e44f2935315faf0f45da19e94445785
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-28 00:59:09 +0700

    Manually triggering validation

--- src/components/4.video-20-keatas/YoutubeForm.jsx
+++ src/components/4.video-20-keatas/YoutubeForm.jsx
@@ -26,23 +26,27 @@ const initialValues = {
 };
 
 // Validate with yup package
-const validationSchema = Yup.object({
+const validationSchema = Yup.object().shape({
   name: Yup.string().required("Name is required!"),
   email: Yup.string()
     .required("Email is required!")
     .email("Email is not valid!"),
   description: Yup.string().max(150, "Description too long (max 150)!"),
   address: Yup.string().required("Address is required!"),
+  social: Yup.object({
+    facebook: Yup.string().url("Facebook is not valid url!"),
+    twitter: Yup.string().url("Twitter is not valid url!"),
+  }),
 });
 
-// Manual validate
-const validateChannel = value => {
+// Manual validate for channel
+const validateChannel = (value) => {
   let error = null;
-  if(!value) {
-    error = "Channel is required!"
+  if (!value) {
+    error = "Channel is required!";
   }
-  return error
-}
+  return error;
+};
 
 const onSubmit = (values) => {
   // e.preventDefault sudah otomatis diatasi oleh formik
@@ -61,159 +65,211 @@ const YoutubeForm = () => {
       // validateOnChange={false}
       // validateOnBlur={false}
     >
-      <Form className='form'>
-        <div>
-          <label htmlFor='name'>Name</label>
-          <Field type='text' name='name' id='name' />
-          <ErrorMessage
-            name='name'
-            component={TextError}
-            placeholder='Input name'
-          />
-        </div>
+      {(formik) => {
+        console.log(formik);
 
-        <div>
-          <label htmlFor='email'>Email</label>
-          <Field type='email' name='email' id='email' />
-          <ErrorMessage name='email'>
-            {(errorMsg) => <div className='error-msg'>{errorMsg}</div>}
-          </ErrorMessage>
-        </div>
+        return (
+          <Form className='form'>
+            <div>
+              <label htmlFor='name'>Name</label>
+              <Field type='text' name='name' id='name' />
+              <ErrorMessage
+                name='name'
+                component={TextError}
+                placeholder='Input name'
+              />
+            </div>
 
-        <div>
-          <label htmlFor='channel'>Channel</label>
-          <Field type='text' name='channel' id='channel' validate={validateChannel} />
-          <div className='error-msg'>
-            <ErrorMessage name='channel' />
-          </div>
-        </div>
+            <div>
+              <label htmlFor='email'>Email</label>
+              <Field type='email' name='email' id='email' />
+              <ErrorMessage name='email'>
+                {(errorMsg) => <div className='error-msg'>{errorMsg}</div>}
+              </ErrorMessage>
+            </div>
 
-        <div>
-          <label htmlFor='description'>Description</label>
-          <Field as='textarea' name='description' id='description' />
-          <div className='error-msg'>
-            <ErrorMessage name='description' />
-          </div>
-        </div>
+            <div>
+              <label htmlFor='channel'>Channel</label>
+              <Field
+                type='text'
+                name='channel'
+                id='channel'
+                validate={validateChannel}
+              />
+              <div className='error-msg'>
+                <ErrorMessage name='channel' />
+              </div>
+            </div>
 
-        <div>
-          <label htmlFor='address'>Address</label>
-          <FastField name='address'>
-            {({ field, form, meta }) => {
-              return (
-                <>
-                  <input type='text' id='address' {...field} />
-                  {meta.touched && meta.error && (
-                    <div className='error-msg'>{meta.error}</div>
-                  )}
-                </>
-              );
-            }}
-          </FastField>
-        </div>
+            <div>
+              <label htmlFor='description'>Description</label>
+              <Field as='textarea' name='description' id='description' />
+              <div className='error-msg'>
+                <ErrorMessage name='description' />
+              </div>
+            </div>
 
-        <div>
-          <label htmlFor='skills'>Skills</label>
-          <FieldArray type='text' name='skills' id='skills'>
-            {({
-              push,
-              remove,
-              form: {
-                values: { skills },
-                errors,
-              },
-            }) => {
-              return (
-                <>
-                  {skills.map((skill, index) => (
-                    <div className='flex-arr' key={index}>
-                      <Field className='input-arr' name={`skills[${index}]`} />
-                      {index === 0 && (
-                        <button
-                          className='btn-arr'
-                          type='button'
-                          onClick={() => push("")}
-                        >
-                          +
-                        </button>
+            <div>
+              <label htmlFor='address'>Address</label>
+              <FastField name='address'>
+                {({ field, form, meta }) => {
+                  return (
+                    <>
+                      <input type='text' id='address' {...field} />
+                      {meta.touched && meta.error && (
+                        <div className='error-msg'>{meta.error}</div>
                       )}
-                      {skills.length > 1 && (
-                        <button
-                          className='btn-arr'
-                          type='button'
-                          onClick={() => remove(index)}
-                        >
-                          -
-                        </button>
-                      )}
-                    </div>
-                  ))}
-                </>
-              );
-            }}
-          </FieldArray>
-          <ErrorMessage
-            name='skills'
-            component={TextError}
-            placeholder='Input skills'
-          />
-        </div>
-
-        <button
-          className='button'
-          type='button'
-          onClick={() => setSocial((e) => !e)}
-        >
-          {!social ? "Add Social" : "Close"}
-        </button>
-        <br />
-
-        {social && (
-          <>
-            <div>
-              <label htmlFor='facebook'>Facebook</label>
-              <Field type='text' name='social.facebook' id='facebook' />
-              <ErrorMessage name='social.facebook' component={TextError} />
+                    </>
+                  );
+                }}
+              </FastField>
             </div>
-            <div>
-              <label htmlFor='twitter'>Twitter</label>
-              <Field type='text' name='social.twitter' id='twitter' />
-              <ErrorMessage name='social.twitter' component={TextError} />
-            </div>
-          </>
-        )}
 
-        <button
-          className='button'
-          type='button'
-          onClick={() => setPhone((e) => !e)}
-        >
-          {!phone ? "Add Phone" : "Close"}
-        </button>
-        <br />
-
-        {phone && (
-          <>
             <div>
-              <label htmlFor='phone1'>Phone 1</label>
-              <Field type='number' name='phone[0]' id='phone1' />
-              <ErrorMessage name='phone[0]' component={TextError} />
+              <label htmlFor='skills'>Skills</label>
+              <FieldArray type='text' name='skills' id='skills'>
+                {({
+                  push,
+                  remove,
+                  form: {
+                    values: { skills },
+                    errors,
+                  },
+                }) => {
+                  return (
+                    <>
+                      {skills.map((skill, index) => (
+                        <div className='flex-arr' key={index}>
+                          <Field
+                            className='input-arr'
+                            name={`skills[${index}]`}
+                          />
+                          {index === 0 && (
+                            <button
+                              className='btn-arr'
+                              type='button'
+                              onClick={() => push("")}
+                            >
+                              +
+                            </button>
+                          )}
+                          {skills.length > 1 && (
+                            <button
+                              className='btn-arr'
+                              type='button'
+                              onClick={() => remove(index)}
+                            >
+                              -
+                            </button>
+                          )}
+                        </div>
+                      ))}
+                    </>
+                  );
+                }}
+              </FieldArray>
+              <ErrorMessage
+                name='skills'
+                component={TextError}
+                placeholder='Input skills'
+              />
             </div>
-            <div>
-              <label htmlFor='phone2'>Phone 2</label>
-              <Field type='number' name='phone[1]' id='phone2' />
-              <ErrorMessage name='phone[1]' component={TextError} />
-            </div>
-          </>
-        )}
 
-        <button className='button' type='submit'>
-          Submit
-        </button>
-        <button className='button' type='reset'>
-          Reset
-        </button>
-      </Form>
+            <button
+              className='button'
+              type='button'
+              onClick={() => setSocial((e) => !e)}
+            >
+              {!social ? "Add Social" : "Close"}
+            </button>
+            <br />
+
+            {social && (
+              <>
+                <div>
+                  <label htmlFor='facebook'>Facebook</label>
+                  <Field type='text' name='social.facebook' id='facebook' />
+                  <ErrorMessage name='social.facebook' component={TextError} />
+                </div>
+                <div>
+                  <label htmlFor='twitter'>Twitter</label>
+                  <Field type='text' name='social.twitter' id='twitter' />
+                  <ErrorMessage name='social.twitter' component={TextError} />
+                </div>
+              </>
+            )}
+
+            <button
+              className='button'
+              type='button'
+              onClick={() => setPhone((e) => !e)}
+            >
+              {!phone ? "Add Phone" : "Close"}
+            </button>
+            <br />
+
+            {phone && (
+              <>
+                <div>
+                  <label htmlFor='phone1'>Phone 1</label>
+                  <Field type='number' name='phone[0]' id='phone1' />
+                  <ErrorMessage name='phone[0]' component={TextError} />
+                </div>
+                <div>
+                  <label htmlFor='phone2'>Phone 2</label>
+                  <Field type='number' name='phone[1]' id='phone2' />
+                  <ErrorMessage name='phone[1]' component={TextError} />
+                </div>
+              </>
+            )}
+
+            <button
+              className='button'
+              type='button'
+              onClick={() => formik.validateField("channel")}
+            >
+              Validate Channel
+            </button>
+            <button
+              className='button'
+              type='button'
+              onClick={() => formik.validateForm()}
+            >
+              Validate All
+            </button>
+            <br />
+
+            <button
+              className='button'
+              type='button'
+              onClick={() => formik.setFieldTouched("channel")}
+            >
+              Touch Channel
+            </button>
+            <button
+              className='button'
+              type='button'
+              onClick={() =>
+                formik.setTouched({
+                  ...formik.touched,
+                  name: true,
+                  email: true,
+                })
+              }
+            >
+              Touch Multiple Field
+            </button>
+            <br />
+
+            <button className='button' type='submit'>
+              Submit
+            </button>
+            <button className='button' type='reset'>
+              Reset
+            </button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
